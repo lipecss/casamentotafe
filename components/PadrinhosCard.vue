9e862e2c5ba4c487807e3db1765ba78e77af200a
santos <santos@4vnts.com.br>
2023-03-23 20:14:27 -0300

    shoiwng friends & family

--- components/PadrinhosCard.vue
+++ components/PadrinhosCard.vue
@@ -3,10 +3,10 @@
     <div class="h-auto bg-cover bg-center" :style="backgroundStyle"></div>
       <div class="p-8">
         <div class="mt-8 mb-8">
-      <h1 class="text-2xl font-black uppercase text-right mb-2">{{ title }}</h1>
-      <p class="text-right">
-        {{  description }}
-      </p>
+          <h1 class="text-2xl font-black uppercase text-right mb-2">{{ name }}</h1>
+          <p class="text-right">
+            {{ description }}
+          </p>
         </div>
     </div>
   </div>
@@ -14,7 +14,7 @@
   <div v-else class="grid grid-cols-2">
     <div class="p-8">
       <div class="mt-8 mb-8">
-        <h1 class="text-2xl font-black uppercase text-left mb-2">{{ title }}</h1>
+        <h1 class="text-2xl font-black uppercase text-left mb-2">{{ name }}</h1>
         <p class="text-left">
           {{  description }}
         </p>
@@ -26,8 +26,6 @@
 </template>
 
 <script setup>
-const images = import.meta.glob('~/assets/img/padrinhos/*', { eager: true })
-
 // props
 const props = defineProps({ 
   position: { 
@@ -37,43 +35,20 @@ const props = defineProps({
     }
   },
   description: String,
-  title: String,
-  filename: { type: String, default: '' },
-  filePosition: { type: String, default: 'center' }
+  name: String,
+  image: { type: String, default: '' },
+  position: { type: String, default: 'center' }
 })
-
-// datas
-const image = ref(null)
 
 // computeds
 const backgroundStyle = computed(() => {
   return {
-    backgroundImage: `url(${image.value})`,
+    backgroundImage: `url(${props.image})`,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
-    'background-position-y': props.filePosition
+    'background-position-y': props.position
   }
 })
-
-// methods 
-function findImage() {
-  const imageName = props.filename.split('.')[0]
-
-  const extensions = ['png', 'jpg', 'jpeg', 'gif']
-  for (const ext of extensions) {
-    const key = `/assets/img/padrinhos/${imageName}.${ext}`
-    if (images[key]) {
-      image.value = images[key].default
-      break
-    }
-  }
-}
-
-
-if (props.filename) {
-  findImage()
-}
-// const image = ref(images.value[props.filename])
 </script>
 
 <style scoped>
