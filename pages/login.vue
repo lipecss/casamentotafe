4ef7b9347600fa23d7bb69036b52695d09481f35
santos <felipecss@vemlavaralouca.com.br>
2023-06-15 15:47:13 -0300

    showing loading on login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,73 +1,76 @@
 <template>
-  <div class="h-screen flex bg-white text-slate-900 items-center justify-center">
-    <div class="w-screen sm:w-11/12 lg:w-1/2">
-      <div class="px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-2xl">
-        <div class="my-16">
-          <NuxtLink to="/"
-            class="lg:hidden p-1 w-auto text-gray-700 cursor-pointer text-center hover:bg-gray-700 hover:text-white">
-            Voltar ao Início
-          </NuxtLink>
-        </div>
-        <h2 class="text-center text-4xl font-display font-semibold lg:text-left xl:text-5xl
-                                                                                            xl:text-bold">
-          Entrar
-        </h2>
+  <div>
+    <Loader v-if="isLoading" />
+    <div class="h-screen flex bg-white text-slate-900 items-center justify-center">
+      <div class="w-screen sm:w-11/12 lg:w-1/2">
+        <div class="px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-2xl">
+          <div class="my-16">
+            <NuxtLink to="/"
+              class="lg:hidden p-1 w-auto text-gray-700 cursor-pointer text-center hover:bg-gray-700 hover:text-white">
+              Voltar ao Início
+            </NuxtLink>
+          </div>
+          <h2 class="text-center text-4xl font-display font-semibold lg:text-left xl:text-5xl
+                                                                                              xl:text-bold">
+            Entrar
+          </h2>
 
-        <h3 class="mt-10 font-bold">Família, padrinhos e convidados</h3>
+          <h3 class="mt-10 font-bold">Família, padrinhos e convidados</h3>
 
-        <p>
-          Utilize seu e-mail informado. Sua senha padrão é o prefixo do seu e-mail até antes do @.
-        </p>
+          <p>
+            Utilize seu e-mail informado. Sua senha padrão é o prefixo do seu e-mail até antes do @.
+          </p>
 
-        <div class="mt-12">
-          <form @submit.prevent="onSubmit">
-            <label class="font-medium block mb-1 mt-6 text-gray-700">
-              E-mail
-            </label>
+          <div class="mt-12">
+            <form @submit.prevent="onSubmit">
+              <label class="font-medium block mb-1 mt-6 text-gray-700">
+                E-mail
+              </label>
 
-            <div class="relative w-full">
-              <input v-model="form.email" class="w-full py-3 px-3 text-lg rounded border border-tuscany" type="text"
-                placeholder="Informe seu e-mail" />
-            </div>
-
-            <label class="font-medium block mb-1 mt-6 text-gray-700" for="password">
-              Senha
-            </label>
-
-            <div class="relative w-full">
-              <div class="absolute inset-y-0 right-0 flex items-center px-2">
-                <input class="hidden js-password-toggle" id="toggle" type="checkbox" />
-                <label
-                  class="bg-tuscany hover:bg-gray-400 rounded px-2 py-1 text-sm text-white font-mono cursor-pointer js-password-label"
-                  for="toggle" @click="toggleShowPassword">
-                  {{ showPasswordText }}
-                </label>
+              <div class="relative w-full">
+                <input v-model="form.email" class="w-full py-3 px-3 text-lg rounded border border-tuscany" type="text"
+                  placeholder="Informe seu e-mail" />
               </div>
 
-              <input v-model="form.password" class="w-full py-3 px-3 text-lg rounded border border-tuscany"
-                :type="passwordInputType" placeholder="Informe sua senha" autocomplete="off" />
-            </div>
+              <label class="font-medium block mb-1 mt-6 text-gray-700" for="password">
+                Senha
+              </label>
 
-            <div class="mt-10">
-              <button
-                class="disabled:opacity-25 bg-tuscany p-4 w-full rounded-full text-white font-semibold focus:shadow-outline hover:bg-pastel-pink"
-                type="submit" :disabled="!isComplete || isLoading">
-                Entrar
-              </button>
-            </div>
-          </form>
+              <div class="relative w-full">
+                <div class="absolute inset-y-0 right-0 flex items-center px-2">
+                  <input class="hidden js-password-toggle" id="toggle" type="checkbox" />
+                  <label
+                    class="bg-tuscany hover:bg-gray-400 rounded px-2 py-1 text-sm text-white font-mono cursor-pointer js-password-label"
+                    for="toggle" @click="toggleShowPassword">
+                    {{ showPasswordText }}
+                  </label>
+                </div>
+
+                <input v-model="form.password" class="w-full py-3 px-3 text-lg rounded border border-tuscany"
+                  :type="passwordInputType" placeholder="Informe sua senha" autocomplete="off" />
+              </div>
+
+              <div class="mt-10">
+                <button
+                  class="disabled:opacity-25 bg-tuscany p-4 w-full rounded-full text-white font-semibold focus:shadow-outline hover:bg-pastel-pink"
+                  type="submit" :disabled="!isComplete || isLoading">
+                  Entrar
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="hidden lg:flex items-center justify-center relative flex-1 h-screen"
-      style="border-radius: 130px 0 0 130px;">
-      <div class="overlay"></div>
+      <div class="hidden lg:flex items-center justify-center relative flex-1 h-screen"
+        style="border-radius: 130px 0 0 130px;">
+        <div class="overlay"></div>
 
-      <NuxtLink to="/" class="p-1 cursor-pointer bt-back hover:bg-gray-700">Voltar ao Início</NuxtLink>
+        <NuxtLink to="/" class="p-1 cursor-pointer bt-back hover:bg-gray-700">Voltar ao Início</NuxtLink>
 
-      <nuxt-img format="webp" class="image" src="https://i.imgur.com/jnV7t17.jpg" alt="Cover login image"
-        title="Cover login image" width="692" height="270" sizes="sm:100vw md:100vw lg:100vw" />
+        <nuxt-img format="webp" class="image" src="https://i.imgur.com/jnV7t17.jpg" alt="Cover login image"
+          title="Cover login image" width="692" height="270" sizes="sm:100vw md:100vw lg:100vw" />
+      </div>
     </div>
   </div>
 </template>
